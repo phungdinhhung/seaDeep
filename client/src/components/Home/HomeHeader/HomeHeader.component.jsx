eab3d8bf659b5bfcdf5ca7ae2e281cd113bc485d
phungdinhhung <63142555+phungdinhhung@users.noreply.github.com>
2023-09-10 21:35:17 +0700

    homeHeader video part 1

--- client/src/components/Home/HomeHeader/HomeHeader.component.jsx
+++ client/src/components/Home/HomeHeader/HomeHeader.component.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import styles from "./HomeHeader.module.scss"
+import { Box } from "@mui/material"
 
 const HomeHeader = () => {
     return (
-        <h2>Home Header</h2>
+        <Box className={styles["header"]} sx={{flexGrow: 1}}>
+            <div className={styles["header_video"]}>
+            <video
+            src="https://video.wixstatic.com/video/11062b_ea61c0ecc8b34ecd942c42470507e640/720p/mp4/file.mp4" 
+            autoPlay 
+            loop
+            muted
+            />
+            </div>
+        </Box>
     )
 }
 
